68e3ee3170f0eb5cd3a07224c2e1e6d73e74c57a
Herb Torres <herb.torres@byhook.com>
2016-04-15 17:13:53 -0400

    fp presentation v0.0.2

--- functional-programming/js/main.build.js
+++ functional-programming/js/main.build.js
@@ -3,8 +3,51 @@
 
 var config = require('./config');
 
-console.log(config);
+var LoadImages = LoadImages || function(a, d, z) {
+	a instanceof Array || (a = [a]);
+	for (var e = a.length, f = 0, g = e; g--;) {
+		var b = document.createElement("img");
+		b.onload = function() {
+			f++; 
+			f >= e && isFunction(d) && d(z)
+		};
+		b.src = a[g]; 
+	}
+};
 
+var isFunction = isFunction || function(functionToCheck) {
+	var getType = {};
+	return functionToCheck && getType.toString.call(functionToCheck) == '[object Function]';
+};
+
+LoadImages([
+	'images/advantage.jpg',
+	'images/core.jpg',
+	'images/frameworks.png',
+	'images/functions-in-out.png',
+	'images/funk.jpg',
+	'images/pulp.png',
+	'images/robit.png',
+	'images/blah.jpg',
+	'images/refactor.jpg',
+	'images/thou.jpg',
+	'images/fist-pump.jpg',
+	'images/PureMichigan.jpg',
+	'images/fp-dino.jpg',
+	'images/mkay.jpg',
+	'images/end-fp.jpg',
+	'images/fighting.gif',
+	'images/goku.gif',
+	'images/library.gif',
+	'images/gage.jpg'
+], imagesAreLoaded);
+
+function imagesAreLoaded() {
+
+	document.querySelector('#loadingOverlay').style.display = 'none';
+
+	console.log('images are loaded');
+};
 },{"./config":2}],2:[function(require,module,exports){
 'use strict';
 
